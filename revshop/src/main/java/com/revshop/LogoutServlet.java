2a53bdc0ff866f01ba29b73b0cc5de507b6f5fe2
jookyjui <juikulkarni1998@gmail.com>
2024-09-01 15:45:09 +0530

    updatedAgain

--- revshop/src/main/java/com/revshop/LogoutServlet.java
+++ revshop/src/main/java/com/revshop/LogoutServlet.java
@@ -4,6 +4,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 
 //@WebServlet("/logout")
@@ -12,12 +14,17 @@ public class LogoutServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
         
-    	 request.getSession().invalidate();
-    	 System.out.println("After session invalidation");
-         // Set cache control headers to prevent caching
-         response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
-         response.setHeader("Pragma", "no-cache");
-         response.setDateHeader("Expires", 0);
+    	// Invalidate the session
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+
+        // Set headers to prevent caching
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+        response.setDateHeader("Expires", 0);
+
 
          // Redirect to the homepage or login page
          response.sendRedirect("index.jsp");
